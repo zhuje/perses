5e4921345b3b14304a829d0710217845754ab8c1
Jenny Zhu <jenny.a.zhu@gmail.com>
2023-12-06 15:49:33 -0500

    stash - everything is done here - need to stash to test working with Apache eChart dataset.source https://echarts.apache.org/en/option.html#dataset.source

--- ui/panels-plugin/src/plugins/scatterplot/ScatterChartPanel.tsx
+++ ui/panels-plugin/src/plugins/scatterplot/ScatterChartPanel.tsx
@@ -6,43 +6,15 @@ import { ScatterChartOptions } from './scatter-chart-model';
 import { Scatterplot } from './Scatterplot';
 import { useSuggestedStepMs } from './utils';
 import { TimeSeriesQueryDefinition } from '@perses-dev/core';
-import { dataTool } from 'echarts';
 import { useDataQueries } from '@perses-dev/plugin-system';
-
 
 
 export type ScatterChartPanelProps = PanelProps<ScatterChartOptions>;
 
 export function ScatterChartPanel(props: ScatterChartPanelProps) {
   const {
-    // spec: { query },
     contentDimensions,
   } = props;
-  const suggestedStepMs = useSuggestedStepMs(contentDimensions?.width) * 10; // temp calc for demo
-
-
-
-  const query =  {
-    kind: 'TimeSeriesQuery',
-    spec: {
-      plugin: {
-        kind: 'PrometheusTimeSeriesQuery',
-        spec: {
-          query:
-            'avg without (cpu)(rate(node_cpu_seconds_total{job=~"node|alertmanager",instance="demo.do.prometheus.io:9100",mode=~"user|idle"}[30m]))',
-        },
-      },
-    },
-  } as TimeSeriesQueryDefinition
-
- // const { data, isLoading, error } = useTimeSeriesQuery(query, { suggestedStepMs });
-  // console.log('JZ query data: ', data)
-
-  
-
-
-  
-
 
   // ScatterPlot Todos 
   // 1. visiualize duration vs time (unix convert to current local time, see TimeSeriesPanel)
@@ -57,64 +29,7 @@ export function ScatterChartPanel(props: ScatterChartPanelProps) {
   // a TraceQuery and TimeSeriesQuery. Isn't it mutally exclusive -- you either have 
   // TraceQuery OR a TimeSeriesQuery?
   const { queryResults: traceResults, isLoading: traceIsLoading } = useDataQueries('TraceQuery');
-  const { queryResults: timeSeriesResults } = useDataQueries('TimeSeriesQuery');
-
-  console.log('JZ /apple traceResults: ', traceResults)
-  console.log('JZ /apple traceResults[0].data: ', traceResults[0]?.data)
-  console.log('JZ /apple timeSeriesResults: ', timeSeriesResults)
-  // console.log('JZ /apple from useTimeSeriesQuery >  data: ', JSON.stringify(data, null, 2) )
-
-
-  // const scatterData = useMemo(() => {
-  //   if (data === undefined) {
-  //     return [];
-  //   }
-  //   const seriesData: ScatterSeriesOption[] = [];
-  //   for (const timeSeries of data.series) {
-  //     const formattedSeriesName = timeSeries.formattedName ?? timeSeries.name;
-  //     let timeSeriesValues = [['timestamp', 'value'], ...timeSeries.values];
-  //     const scatterSeries: ScatterSeriesOption = {
-  //       type: 'scatter', // https://echarts.apache.org/en/option.html#series-scatter.type
-  //       name: formattedSeriesName,
-  //       data: timeSeriesValues,
-  //       symbolSize: 4,
-  //     };
-  //     seriesData.push(scatterSeries);
-  //   }
-  //   console.log('JZ /pie seriesData: ', seriesData);
-  //   return seriesData;
-  // }, [data]);
-
-
-  //   /**
-  //  * formatTimeStamp() - converts Tempo trace data attribute 'startTimeUnixNano'
-  //  * into Coordinated Universal Time (UTC)
-  //  */
-  // function formatTimeStamp(nanosecondsStr: string) {
-
-  //   const nanoseconds = parseInt(nanosecondsStr)
-  //   const miliseconds = nanoseconds * 1e-6
-
-  //   const dtFormat = new Intl.DateTimeFormat('en-GB', {
-  //     timeStyle: 'medium',
-  //     timeZone: 'UTC'
-  //   });
-
-  //   return dtFormat.format(new Date(miliseconds));
-  // }
-
-  function convertNanoToMiliseconds(nanosecondsStr: string){
-    const nanoseconds = parseInt(nanosecondsStr)
-    const miliseconds = nanoseconds * 1e-6
-    return miliseconds
-  }
-
-  function convertUnixMsToLocalTime(TimeUnixMs: number){
-    var date = new Date(TimeUnixMs);
-    const time = date.toLocaleTimeString("it-IT")
-    console.log(time);
-    return time
-  }
+  const { queryResults: timeSeriesResults, isLoading: timeSeriesIsLoading } = useDataQueries('TimeSeriesQuery');
 
   const scatterTraceData = useMemo(() => {
     const traceData = traceResults[0]?.data
@@ -126,10 +41,11 @@ export function ScatterChartPanel(props: ScatterChartPanelProps) {
     const seriesData: ScatterSeriesOption[] = [];
     const traceDurations = []
     for (let trace of traceData.traces) {
+
+        console.log("trace: ", trace)
+
         let startTimeUnixMs = new Date();
-        startTimeUnixMs = new Date(trace.startTimeUnixMs)
-        console.log('JZ /cheesecake : startTimeUnixMs ', trace.startTimeUnixMs, startTimeUnixMs)
-        
+        startTimeUnixMs = new Date(trace.startTimeUnixMs)        
         const duration =  trace?.durationMs!
         const spanCount = trace?.spanCount!
         const errorCount = trace?.errorCount!
@@ -166,12 +82,13 @@ export function ScatterChartPanel(props: ScatterChartPanelProps) {
       },
       itemStyle: {
         color: function(params){ // https://stackoverflow.com/questions/56715577/scatter-plot-with-colored-markers-colormap-in-echarts
-          const dataValues = params?.data as Array<[Date, number,number, number]>
-          console.log("dataValues[3]", dataValues[3])
+          const dataValues = params?.data as [Date, number,number, number]
+          // If there is an error in a span, color the datapoint red 
           const errorCount =  dataValues[3]
-          // if (errorCount !== undefined && errorCount > 0) {
-          //   return 'red'
-          // }
+          if (errorCount !== undefined && errorCount > 0) {
+            return 'red'
+          }
+          // Else return default color
           const defaultColor = "#56B4E9"
           const dataPointColor = (params.color !== undefined) ? params.color : defaultColor
           return dataPointColor
